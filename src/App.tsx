46cf5fdd1147f32d2443c774f2ce76f0d945e43b
Joe Simpson <the.fluff.lives.on@gmail.com>
2025-10-08 16:55:51 +0100

    set up mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { type FC } from 'react';
+import { AppShell, Burger, createTheme } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineProvider } from '@mantine/core';
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: FC = () => {
+  const theme = createTheme({});
+  const [opened, { toggle }] = useDisclosure();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider theme={theme}>
+    <AppShell
+      padding="md"
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="sm"
+        />
+        <div>Logo</div>
+      </AppShell.Header>
+      <AppShell.Navbar>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="sm"
+        /></AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+    </MantineProvider>
   )
 }
 
